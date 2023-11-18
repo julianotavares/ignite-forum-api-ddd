6ed2a19c6e5d6175a9250c52cf921778f8af1e72
Juliano Tavares <juliano.tavaress@gmail.com>
2023-11-18 14:11:03 -0300

    chore(abstraction): create entities abstraction

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 import { Answer } from "../entities/answer";
 import { AnswerRepository } from "../repositories/answers-repository";
 
@@ -12,10 +13,10 @@ export class AnswerQuestionUseCase {
     private answerRepository: AnswerRepository
   ) {}
   async execute({ instructorId, questionId, content}: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer({
+    const answer = Answer.create({
       content,
-      authorId: instructorId,
-      questionId,
+      authorId: new UniqueEntityID(instructorId),
+      questionId: new UniqueEntityID(questionId)
     })
 
     await this.answerRepository.create(answer)
